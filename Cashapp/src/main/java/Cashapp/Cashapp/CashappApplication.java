111344b7094c240eb3591aef1a36b55937c61c88
abzoghlami <abzoghlami@a112pc17L.local.isima.fr>
2024-02-14 11:03:09 +0100

    init

---
+++ Cashapp/src/main/java/Cashapp/Cashapp/CashappApplication.java
@@ -0,0 +1,13 @@
+package Cashapp.Cashapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashappApplication.class, args);
+	}
+
+}
---
+++ Cashapp/src/test/java/Cashapp/Cashapp/CashappApplicationTests.java
@@ -0,0 +1,13 @@
+package Cashapp.Cashapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
