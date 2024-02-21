a89b41c3702e12e6d1c79948f1efdbff334893ed
ZOGHLAMI Abdeljalil <81550632+SomeOne768@users.noreply.github.com>
2024-02-21 11:50:15 +0100

    adding index controller and basic page

---
+++ CashApp/src/main/java/app/controllers/IndexController.java
@@ -0,0 +1,21 @@
+package app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+
+@Controller    
+public class IndexController {
+
+
+  @GetMapping(path={"/"})
+  public String index() {
+
+    return "index"; 
+  }
+}
