0cd9ce071aab7f63d58ac3508c9246d3606f691f
Remi Giraudon <giraudon.remi@gmail.com>
2024-03-19 16:17:57 +0100

    Recherche d'objets depuis index

--- CashApp/src/main/java/app/controllers/IndexController.java
+++ CashApp/src/main/java/app/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package app.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import app.entities.Product;
 import app.repositories.ProductRepository;
+import app.services.ProductService;
 
 import org.springframework.ui.Model;
 
@@ -18,10 +22,28 @@ public class IndexController {
   @Autowired
   private ProductRepository productRepository;
 
+   @Autowired
+    private ProductService productService;
+
   @GetMapping(path={"/"})
   public String index(Model model) {
     model.addAttribute("searchedProducts", productRepository.findAll());
     return "index"; 
+  }
+
+  // Permet de voir tous les produits disponible à l'achat
+  @GetMapping(path = { "/index/GetProducts/" })
+  public String findProduct(@RequestParam(name = "query", required = false) String query, Model model) {
+    ArrayList<Product> products = productService.findProductsByName(query);
+      System.out.println("\n\n\n AAAA \n\n\n");
+
+      for (Product product : products) {
+        System.out.println(product.getImageUrl() + "\n");
+      }
+      
+
+      model.addAttribute("searchedProducts", products);
+      return "index";
   }
 
   @GetMapping(path = { "/secu" })
