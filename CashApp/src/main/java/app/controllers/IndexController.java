1477eae8cb2f49cd946eac859310c40daebeeb1e
Remi Giraudon <giraudon.remi@gmail.com>
2024-03-19 22:10:14 +0100

    Ajout du formulaire pour chercher des produits

--- CashApp/src/main/java/app/controllers/IndexController.java
+++ CashApp/src/main/java/app/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package app.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,25 +10,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import app.entities.Product;
 import app.repositories.ProductRepository;
+import app.services.ProductService;
 
 import org.springframework.ui.Model;
 
 @Controller
 public class IndexController {
 
-  @Autowired
-  private ProductRepository productRepository;
+	@Autowired
+	private ProductRepository productRepository;
 
-  @GetMapping(path={"/"})
-  public String index(Model model) {
-    model.addAttribute("searchedProducts", productRepository.findAll());
-    return "index"; 
-  }
+	@Autowired
+	private ProductService productService;
 
-  @GetMapping(path = { "/secu" })
-  public String secu() {
+	@GetMapping(path = { "/" })
+	public String index(Model model) {
+		model.addAttribute("searchedProducts", productRepository.findAll());
+		return "index";
+	}
 
-    return "secu.html";
-  }
+	// Permet de voir tous les produits disponible à l'achat
+	@GetMapping(path = { "/index/GetProducts/" })
+	public String findProduct(@RequestParam(name = "query", required = false) String query, Model model) {
+		ArrayList<Product> products = productService.findProductsByName(query);
+		model.addAttribute("searchedProducts", products);
+		return "index";
+	}
+
+	@GetMapping(path = { "/secu" })
+	public String secu() {
+
+		return "secu.html";
+	}
 }
